1b72f8d6f0120e45d1ef1fd8880dd3fc46d80502
benjamin espeche <benjamin.espeche97@gmail.com>
2023-09-08 18:24:50 +0200

    update app

--- app/app.py
+++ app/app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.write('Hello World!')
+# Page
+st.set_page_config(page_title="Spotify Song Genre Classifier", page_icon=":musical_notes", layout="wide")
+
+
+# HEADER SECTION 
+st.subheader("Let's choose a song and see the model identify a genre")
+st.write("Check out my GitHub repository : https://github.com/Benesp97/spotifysong_genre_classifier")
+
