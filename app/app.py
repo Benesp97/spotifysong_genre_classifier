38f06b015d276b332039557f0317dd88f9ee646c
benjamin espeche <benjamin.espeche97@gmail.com>
2023-09-08 18:26:08 +0200

    try

--- app/app.py
+++ app/app.py
@@ -8,3 +8,4 @@ st.set_page_config(page_title="Spotify Song Genre Classifier", page_icon=":music
 st.subheader("Let's choose a song and see the model identify a genre")
 st.write("Check out my GitHub repository : https://github.com/Benesp97/spotifysong_genre_classifier")
 
+st.write('HellO')
