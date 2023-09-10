dbe87e81ae6ba4f146de7fa4b2a36be2c83e7320
benjamin espeche <benjamin.espeche97@gmail.com>
2023-09-10 17:41:03 +0200

    bla

--- app/app.py
+++ app/app.py
@@ -22,5 +22,7 @@ st.set_page_config(page_title="Spotify Song Genre Classifier", page_icon=":music
 st.subheader("Let's choose a song and see the model identify a genre")
 st.write("Check out my GitHub repository : https://github.com/Benesp97/spotifysong_genre_classifier")
 
-df = pd.read_csv(os.path.join(".", "Data", "spotify_tracks.csv"))
+PATH = os.path.join("..", "Data", "spotify_tracks.csv")
+df = pd.read_csv(PATH)
+# df = pd.read_csv('../Data/spotify_tracks.csv')
 print(df.head(1))
